677142c61f2858af7b9c3be784ffe264aabed8d0
wbaek <wbaek.personal@gmail.com>
2015-11-17 09:58:00 +0000

    update serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from .models import Image, Tag
 
-class TagModelSerializer(serializers.ModelSerializer):
+class _ImageModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+
+class _TagModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
 
 class ImageModelSerializer(serializers.ModelSerializer):
-    tags = TagModelSerializer(many=True, read_only=True)
+    tags = _TagModelSerializer(many=True, read_only=True)
     class Meta:
         model = Image
+
+class TagModelSerializer(serializers.ModelSerializer):
+    image_detail = _ImageModelSerializer(read_only=True, source='image')
+    class Meta:
+        model = Tag
 
