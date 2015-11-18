c81fa5533599afd3c7202cbb5e9200105afb480a
wbaek <wbaek.personal@gmail.com>
2015-11-18 08:20:41 +0000

    add sql cloude connections

--- memespot/settings.py
+++ memespot/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'memespot.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default_sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'memespot',
+        'USER': 'admin',
+        'PASSWORD': 'memespot',
+        'HOST': '173.194.255.92',
     }
 }
 
